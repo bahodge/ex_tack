9a01f098cdf0d3b1742cb2b423cdac721b6d8f9f
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2019-06-18 22:31:21 -0500

    added more tests to formatter and ex tack

--- test/ex_tack_test.exs
+++ test/ex_tack_test.exs
@@ -1,6 +1,65 @@
 defmodule ExTackTest do
+  use ExUnit.Case
 
+  @good_version_1 "v1.23.4"
+  @good_version_2 "v7.0.4"
+  @bad_version "v1.23.4.6.5"
+  @new_line_content "Hello World"
 
+  defp cleanup_release_files do
+    File.rm_rf("lib/releases")
+    :ok
+  end
 
+  setup do
+    ExTack.init()
+    on_exit("Some note", fn -> cleanup_release_files() end)
+    :ok
+  end
 
+  test "create/1" do
+    filename = "#{@good_version_1}_release.md"
+    assert {:error, :enoent} = File.read("lib/releases/test/#{filename}")
+    assert :ok = ExTack.create(@good_version_1)
+    assert {:ok, _content} = File.read("lib/releases/test/#{filename}")
+  end
+
+  test "create/2" do
+    filename = "#{@good_version_1}_release.md"
+    assert {:error, :enoent} = File.read("lib/releases/test/#{filename}")
+    assert :ok = ExTack.create(@good_version_1, @new_line_content)
+    assert {:ok, _content} = File.read("lib/releases/test/#{filename}")
+  end
+
+  test "read/0" do
+    ExTack.create(@good_version_1)
+
+    assert {:ok, _content, filename} = ExTack.read()
+    assert filename == "#{@good_version_1}_release.md"
+  end
+
+  test "read/1" do
+    ExTack.create(@good_version_1)
+    ExTack.create(@good_version_2)
+
+    assert {:ok, _content, filename} = ExTack.read(@good_version_1)
+    assert filename == "#{@good_version_1}_release.md"
+  end
+
+  test "update_or_create/2 -> Creates new file" do
+    assert :ok = ExTack.create(@good_version_1)
+    assert {:ok, content, _filename} = ExTack.read(@good_version_1)
+
+    assert :ok = ExTack.update_or_create(@good_version_1, @new_line_content)
+    {:ok, updated_contents, _filename} = ExTack.read(@good_version_1)
+    assert content != updated_contents
+  end
+
+  test "update_or_create/2 -> Updates existing file" do
+    filename = "#{@good_version_1}_release.md"
+    assert {:error, :not_found} = ExTack.read(@good_version_1)
+    assert :ok = ExTack.update_or_create(@good_version_1, @new_line_content)
+    assert {:ok, _content, new_filename} = ExTack.read(@good_version_1)
+    assert new_filename == filename
+  end
 end
--- test/helpers/version_formatter_test.exs
+++ test/helpers/version_formatter_test.exs
@@ -6,6 +6,8 @@ defmodule Helpers.VersionFormatterTest do
   @good_version_2 "v1.2"
   @good_version_3 "v1"
   @good_version_4 "1"
+  @good_version_5 "v31.2342.55342"
+  @good_version_6 "1.2432.23434"
 
   @bad_version "1.2.3.4"
 
@@ -14,10 +16,14 @@ defmodule Helpers.VersionFormatterTest do
     expected_result_2 = "v1.2.0"
     expected_result_3 = "v1.0.0"
     expected_result_4 = "v1.0.0"
+    expected_result_5 = "v31.2342.55342"
+    expected_result_6 = "v1.2432.23434"
     assert VersionFormatter.format_version(@good_version_1) == expected_result_1
     assert VersionFormatter.format_version(@good_version_2) == expected_result_2
     assert VersionFormatter.format_version(@good_version_3) == expected_result_3
     assert VersionFormatter.format_version(@good_version_4) == expected_result_4
+    assert VersionFormatter.format_version(@good_version_5) == expected_result_5
+    assert VersionFormatter.format_version(@good_version_6) == expected_result_6
     assert {:error, :bad_version} = VersionFormatter.format_version(@bad_version)
   end
 end
